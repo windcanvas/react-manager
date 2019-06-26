f923b35e476d33cca6a49241d4ae960170411612
liqilong <liqilong199126@gmail.com>
2019-06-26 11:52:30 +0800

    基本架构组件

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.less';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                版权所有：wind（推荐使用谷歌浏览器，可以获得更佳操作页面体验） 技术支持：wind
+            </div>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.less';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className='home-wrap'>
+                基于React的共享单车管理系统
+            </div>
+        )
+    }
+}
+
+export default Home;
